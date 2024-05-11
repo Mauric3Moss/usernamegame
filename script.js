d687258204529dfcdc1fd68fc263734f76d3980b
Glitch (glitch-hello-website) <none>
2024-05-11 11:28:37 +0000

    🎤🏓 Checkpoint ./game.html:54135134/841 ./style.css:54135134/107 ./script.js:54135134/2474

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function start() {
   window.location.href = "game.html";
 }
+var game1Ans = "0b"
+var game2Ans = "0b"
+var game3Ans = "0b"
+var game4Ans = "0b"
+
+let correctColor;
 
 function game1() {
-  var color = document.getElementById("colorPicked").value;
   const colors = [
     "#060606", "#F90307", "#E9FFEB", "#626360"
   ];
-  var randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.querySelector(".col").style.backgroundColor = randomColor;
+  correctColor = colors[Math.floor(Math.random() * colors.length)];
+  document.querySelector(".col").style.backgroundColor = correctColor;
+}
+
+function checkColor() {
+  const userColor = document.getElementById("colorPicked").value;
+  if (userColor === correctColor) {
+    game1Ans = "1b"
+  }
 }
