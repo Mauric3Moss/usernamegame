b561f8e5c618a3baac537fe6637c9aa887f04e96
Glitch (glitch-hello-website) <none>
2024-05-11 11:17:46 +0000

    🐉🏆 Checkpoint ./style.css:54135134/52 ./game.html:54135134/1239 ./script.js:54135134/710

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function start(){
-window.location.href = "game.html";
+function start() {
+  window.location.href = "game.html";
 }
-function game1(){
+
+function game1() {
   var color = document.getElementById("colorPicked").value;
   const colors = [
     "#060606", "#F90307", "#E9FFEB", "#626360"
   ];
-  var rightCol = colors[Math.floor(Math.random() * colors.length)];
-  
-  document.getElementById("col").style.color = rightCol;
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.querySelector(".col").style.backgroundColor = randomColor;
 }
