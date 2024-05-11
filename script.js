3a85c41492fab55721a9e490997e7bd94bc2af3d
Glitch (glitch-hello-website) <none>
2024-05-11 19:16:46 +0000

    🎃👧 Checkpoint ./style.css:54135134/271 ./script.js:54135134/1544

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function start() {
   window.location.href = "game.html";
 }
 
-var game1Ans = "0b"
-var game2Ans = "0b"
-var game3Ans = "0b"
-var game4Ans = "0b"
+var game1Ans = "0b";
+var game2Ans = "0b";
+var game3Ans = "0b";
+var game4Ans = "0b";
 
 let correctColor;
 
@@ -17,8 +17,19 @@ function game1() {
 function checkColor() {
   const userInput = document.getElementById("colorInput").value.toUpperCase();
   if (userInput === correctColor.toUpperCase()) {
-    game1Ans = "1b"
+    game1Ans = "1b";
     alert("Correct! You may proceed.");
+    document.getElementById("game2").style.display = "block";
+  } else {
+    alert("Incorrect. Please try again.");
+  }
+}
+
+function checkHiddenInput() {
+  const hiddenInput = document.getElementById("hiddenInput").value;
+  if (hiddenInput === "hidden text") {
+    alert("Correct!");
+    // Add additional logic or proceed to the next level
   } else {
     alert("Incorrect. Please try again.");
   }
