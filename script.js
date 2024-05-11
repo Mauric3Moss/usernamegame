4441ef8d4fed487fa5982fcbc25590eb585dc2f3
Glitch (glitch-hello-website) <none>
2024-05-11 11:06:39 +0000

    🐸📼 Checkpoint ./script.js:54135134/297 ./game.html:54135134/42

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ window.location.href = "game.html";
 }
 function game1(){
   var color = document.getElementById("colorPicked").value;
+  const colors = [
+    "#060606", "#F90307", "#E9FFEB", "#626360"
+  ];
+  var rightCol = colors[Math.floor(Math.random() * colors.length)];
+  
+  document.getElementById("col").style.color = rightCol;
 }
